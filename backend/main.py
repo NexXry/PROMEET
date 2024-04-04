c68cfbb2f107a78aab09b357b9999c43012729f2
nicolas castex <nicolas.castex.pro@gmail.com>
2024-04-04 10:37:28 +0200

    saved

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from http.client import HTTPException
 from pydantic import BaseModel, EmailStr
 from fastapi import FastAPI
 from database import connect, initialize_db, recherche_dans_la_base
+from src.model.Formulaire import Formulaire
 from src.model.User import User
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -21,21 +22,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.on_event("startup")
 async def startup_event():
     initialize_db()
+
 
 @app.get("/recherche")
 async def recherche(q: str):
     result = recherche_dans_la_base(q)
     return {'find': result}
 
+
 @app.get("/users/{userId}")
 async def create_user(userId: int):
     user = findUserById(userId)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return  user
+    return user
 
 
 @app.post("/send_email")
