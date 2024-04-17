fa92601abf20bfbc9382679b4bee09892f72af93
nicolas castex <nicolas.castex.pro@gmail.com>
2024-04-17 09:55:36 +0200

    ajout fonctionnement list RDV

--- frontend/src/Pages/Profil.jsx
+++ frontend/src/Pages/Profil.jsx
@@ -3,8 +3,10 @@ import {ListRdvUser} from "../Composants/ListRdvUser.jsx";
 import {ListRdvAValider} from "../Composants/ListRdvAValider.jsx";
 import {authStore} from "../store/authStore.js";
 import {useParams} from "react-router-dom";
+import {useState} from "react";
 
 export const Profil = () => {
+    const [updated, setUpdated] = useState(false);
     const {auth} = authStore()
     const {id} = useParams();
     return (
@@ -12,8 +14,8 @@ export const Profil = () => {
             <InfoProfil></InfoProfil>
             {auth?.user?.id == id ?
                 <div className={'flex flex-col justify-between gap-10'}>
-                    <ListRdvUser/>
-                    <ListRdvAValider/>
+                    <ListRdvUser updated={updated} setUpdated={setUpdated}/>
+                    <ListRdvAValider updated={updated} setUpdated={setUpdated}/>
                 </div> :
                 ""}
         </>
