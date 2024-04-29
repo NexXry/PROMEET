993d908f139f8e0e843fb13bb7b66591359bdf22
nicolas castex <nicolas.castex.pro@gmail.com>
2024-04-29 15:29:52 +0200

    unif

--- frontend/src/Composants/ButtonPersonneRDV.jsx
+++ frontend/src/Composants/ButtonPersonneRDV.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 export function ButtonPersonneRDV() {
     return (
-        <div className="flex justify-center mt-24">
+        <div className="flex justify-center my-80">
             <div>
                 <Typography className="font-bold text-xl text-bleuFonce text-center mb-6">
                     Accueil Back Office
